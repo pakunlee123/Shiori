e5005d9c70aec873c588ce7d9cda9765d888353c
pakunlee123 <pakunlee@gmail.com>
2026-09-06 21:47:27 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,32 @@
+const CACHE_NAME = "shiori-v1";
+
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon.svg"
 ];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
