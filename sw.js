4be767e3f3ebb43bee2fa3813d26ddfae289bd80
pakunlee123 <pakunlee@gmail.com>
2026-09-06 23:02:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shiori-v5-1";
+const CACHE_NAME = "shiori-v5-2";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
@@ -23,9 +23,11 @@ self.addEventListener("fetch", event => {
     fetch(event.request)
       .then(response => {
         const copy = response.clone();
+
         caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, copy);
         });
+
         return response;
       })
       .catch(() => caches.match(event.request))
